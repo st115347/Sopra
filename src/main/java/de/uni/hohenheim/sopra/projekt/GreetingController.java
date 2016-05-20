93319018bb489a415616f2a5c4af753aca0a0b71
st115347 <tabea.schmid93@web.de>
2016-05-20 22:10:38 +0200

    Willkommensseite überarbeitet

--- src/main/java/de/uni/hohenheim/sopra/projekt/GreetingController.java
+++ src/main/java/de/uni/hohenheim/sopra/projekt/GreetingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="Unbekannter") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
