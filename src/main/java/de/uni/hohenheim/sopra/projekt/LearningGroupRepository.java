578e55bd55e4fa13ed51419cd4f1ee1440c75803
Melo16 <st119014@stud.uni-stuttgart.de>
2016-06-03 17:08:19 +0200

    Klassen bearbeitet

--- src/main/java/de/uni/hohenheim/sopra/projekt/LearningGroupRepository.java
+++ src/main/java/de/uni/hohenheim/sopra/projekt/LearningGroupRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by Meltem on 02.06.2016.
  */
-
 public interface LearningGroupRepository extends JpaRepository<LearningGroup, Integer> {
 }
