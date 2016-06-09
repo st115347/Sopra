da38fbc44bfbd4b1df56aab71b1fa38ca99ba63f
Simon Bumiller <simon.bumiller@wi1.uni-hohenheim.de>
2016-06-09 11:19:29 +0200

    Unnötige Imports entfernt

--- src/main/java/de/uni/hohenheim/sopra/projekt/SopraApplication.java
+++ src/main/java/de/uni/hohenheim/sopra/projekt/SopraApplication.java
@@ -1,22 +1,7 @@
 package de.uni.hohenheim.sopra.projekt;
 
-
-import java.io.File;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-import org.springframework.util.FileSystemUtils;
 @SpringBootApplication
 
 
