d0cd9053973b6aa1b8d27024001c72f67e0d792e
Melo16 <st119014@stud.uni-stuttgart.de>
2016-07-02 14:45:54 +0200

    test

--- src/main/java/de/uni/hohenheim/sopra/projekt/model/Beitrag.java
+++ src/main/java/de/uni/hohenheim/sopra/projekt/model/Beitrag.java
@@ -29,7 +29,7 @@ public class Beitrag implements Serializable{
     private String author;
 
     @Column(name = "name")
-    @Size(min=1, max=50, message="Das Topic muss zwischen 1 und 50 Zeichen lang sein")
+    @Size(min=1, max=50, message="Das Thema muss zwischen einem und 50 Zeichen lang sein")
     private String name;
     @Size(min=12, max=9999, message="Die Nachricht darf nicht leer sein oder länger als 9999 Zeichen")
     @Column(name = "text",length = 10000)
